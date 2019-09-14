542b0098ed8d16b4dfbfd8e584b1c89b06d11bc6
nag5000 <al3x.nag@gmail.com>
2019-09-14 23:52:12 +0500

    Update

--- personal/80-keybindings.el
+++ personal/80-keybindings.el
@@ -126,12 +126,11 @@ T - tag prefix
 (define-key dired-mode-map "." 'hydra-dired/body)
 
 (defhydra hydra-flycheck
-  (:post (quit-windows-on "*Flycheck errors*")
-        :hint nil)
+  (:post (quit-windows-on "*Flycheck errors*") :hint nil :foreign-keys run)
   "Flycheck Errors"
   ("f" flycheck-error-list-set-filter "Filter")
-  ("j" flycheck-next-error "Next")
-  ("k" flycheck-previous-error "Previous")
+  ("]" flycheck-next-error "Next")
+  ("[" flycheck-previous-error "Previous")
   ("gg" flycheck-first-error "First")
   ("G" (progn (goto-char (point-max)) (flycheck-previous-error)) "Last")
   ("q" nil "quit"))
