17e096da73357551055a309797d913502b39a6ac
nag5000 <al3x.nag@gmail.com>
2019-09-14 23:48:47 +0500

    Update

--- personal/80-keybindings.el
+++ personal/80-keybindings.el
@@ -1,5 +1,140 @@
-(prelude-require-package 'mwim)
+(prelude-require-packages '(mwim hydra))
 (require 'mwim)
+
+;; HYDRA DEFS
+(defhydra dumb-jump-hydra (:color blue :columns 3)
+  "Dumb Jump"
+  ("g" dumb-jump-go "Go")
+  ("f" dumb-jump-quick-look "Quick look")
+  ("o" dumb-jump-go-other-window "Other window")
+  ("e" dumb-jump-go-prefer-external "Go external")
+  ("x" dumb-jump-go-prefer-external-other-window "Go external other window")
+  ("i" dumb-jump-go-prompt "Prompt")
+  ("b" dumb-jump-back "Back"))
+
+(defhydra zoom-hydra ()
+  "zoom"
+  ("=" text-scale-increase "in")
+  ("+" text-scale-increase "in")
+  ("-" text-scale-decrease "out")
+  ("_" text-scale-decrease "out")
+  ("0" text-scale-adjust "reset" :exit t)
+  ("q" nil "quit"))
+
+(defun my/name-of-buffers (n)
+  "Return the names of the first N buffers from `buffer-list'."
+  (let ((bns
+         (delq nil
+               (mapcar
+                (lambda (b)
+                  (unless (string-match "^ " (setq b (buffer-name b)))
+                    b))
+                (buffer-list)))))
+    (subseq bns 1 (min (1+ n) (length bns)))))
+
+;; Given ("a", "b", "c"), return "[1] a, [2] b, [3] c".
+(defun my/number-names (list)
+  "Enumerate and concatenate LIST."
+  (let ((i 0))
+    (mapconcat
+     (lambda (x)
+       (format "[%d] %s" (cl-incf i) x))
+     list
+     ", ")))
+
+(defvar my/last-buffers nil)
+
+(defun my/switch-to-buffer (arg)
+  (interactive "p")
+  (switch-to-buffer
+   (nth (1- arg) my/last-buffers)))
+
+(defun my/switch-to-buffer-other-window (arg)
+  (interactive "p")
+  (switch-to-buffer-other-window
+   (nth (1- arg) my/last-buffers)))
+
+;; https://github.com/abo-abo/hydra/wiki/Switch-to-buffer
+(defhydra switch-to-buffer-hydra (:exit nil
+                                        :body-pre (setq my/last-buffers
+                                                        (my/name-of-buffers 6)))
+  "
+Buffers: %s(my/number-names my/last-buffers)
+"
+  ("1" (my/switch-to-buffer 1))
+  ("2" (my/switch-to-buffer 2))
+  ("3" (my/switch-to-buffer 3))
+  ("4" (my/switch-to-buffer 4))
+  ("5" (my/switch-to-buffer 5))
+  ("6" (my/switch-to-buffer 6))
+
+  ;; ("o" my/switch-to-buffer "this window")
+  ;; ("O" my/switch-to-buffer-other-window "other window")
+  ("q" nil "quit"))
+
+(defhydra hydra-dired (:hint nil :color pink)
+  "
+_+_ mkdir          _v_iew           _m_ark             _(_ details        _i_nsert-subdir    wdired
+_C_opy             _O_ view other   _U_nmark all       _)_ omit-mode      _$_ hide-subdir    C-x C-q : edit
+_D_elete           _o_pen other     _u_nmark           _l_ redisplay      _w_ kill-subdir    C-c C-c : commit
+_R_ename           _M_ chmod        _t_oggle           _g_ revert buf     _e_ ediff          C-c ESC : abort
+_Y_ rel symlink    _G_ chgrp        _E_xtension mark   _s_ort             _=_ pdiff
+_S_ymlink          ^ ^              _F_ind marked      _._ toggle hydra   \\ flyspell
+_r_sync            ^ ^              ^ ^                ^ ^                _?_ summary
+_z_ compress-file  _A_ find regexp
+_Z_ compress       _Q_ repl regexp
+
+T - tag prefix
+"
+  ("\\" dired-do-ispell)
+  ("(" dired-hide-details-mode)
+  (")" dired-omit-mode)
+  ("+" dired-create-directory)
+  ("=" diredp-ediff)         ;; smart diff
+  ("?" dired-summary)
+  ("$" diredp-hide-subdir-nomove)
+  ("A" dired-do-find-regexp)
+  ("C" dired-do-copy)        ;; Copy all marked files
+  ("D" dired-do-delete)
+  ("E" dired-mark-extension)
+  ("e" dired-ediff-files)
+  ("F" dired-do-find-marked-files)
+  ("G" dired-do-chgrp)
+  ("g" revert-buffer)        ;; read all directories again (refresh)
+  ("i" dired-maybe-insert-subdir)
+  ("l" dired-do-redisplay)   ;; relist the marked or singel directory
+  ("M" dired-do-chmod)
+  ("m" dired-mark)
+  ("O" dired-display-file)
+  ("o" dired-find-file-other-window)
+  ("Q" dired-do-find-regexp-and-replace)
+  ("R" dired-do-rename)
+  ("r" dired-do-rsynch)
+  ("S" dired-do-symlink)
+  ("s" dired-sort-toggle-or-edit)
+  ("t" dired-toggle-marks)
+  ("U" dired-unmark-all-marks)
+  ("u" dired-unmark)
+  ("v" dired-view-file)      ;; q to exit, s to search, = gets line #
+  ("w" dired-kill-subdir)
+  ("Y" dired-do-relsymlink)
+  ("z" diredp-compress-this-file)
+  ("Z" dired-do-compress)
+  ("q" nil)
+  ("." nil :color blue))
+
+(define-key dired-mode-map "." 'hydra-dired/body)
+
+(defhydra hydra-flycheck
+  (:post (quit-windows-on "*Flycheck errors*")
+        :hint nil)
+  "Flycheck Errors"
+  ("f" flycheck-error-list-set-filter "Filter")
+  ("j" flycheck-next-error "Next")
+  ("k" flycheck-previous-error "Previous")
+  ("gg" flycheck-first-error "First")
+  ("G" (progn (goto-char (point-max)) (flycheck-previous-error)) "Last")
+  ("q" nil "quit"))
 
 ;; GLOBAL KEYINDINGS
 (global-set-key (kbd "<f5>") 'evil-write-all)
@@ -183,6 +318,8 @@
   "bm" 'view-echo-area-messages
   "bD" 'crux-kill-other-buffers
   ";"  'frog-jump-buffer-same-project
+  "b TAB" 'switch-to-buffer-hydra/body
+  "be" 'hydra-flycheck/body
 )
 
 ;; Window
@@ -226,9 +363,10 @@
   "rl" 'helm-resume
 )
 
-;; ?
+;; text
 (evil-leader/set-key
   "xa" 'align-regexp
+  "xg" 'dumb-jump-hydra/body
 )
 
 ;; VCS
@@ -255,6 +393,11 @@
   ;; highlight-changes-mode
   "hct" 'highlight-changes-visible-mode
   "hcc" (lambda () (interactive) (highlight-changes-remove-highlight (point-min) (point-max)))
+)
+
+;; Toggle
+(evil-leader/set-key
+  "tz" 'zoom-hydra/body
 )
 
 ;; Quit
