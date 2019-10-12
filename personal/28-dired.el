a66d74b980fe54b5c6e7988db2b8a0565fa7d36e
nag5000 <al3x.nag@gmail.com>
2019-10-12 22:55:18 +0500

    Install dired-git-info

--- personal/28-dired.el
+++ personal/28-dired.el
@@ -1,5 +1,9 @@
 ;; https://github.com/Fuco1/dired-hacks
-(prelude-require-packages '(dired-collapse))
+;; https://github.com/clemera/dired-git-info
+(prelude-require-packages '(dired-collapse dired-git-info))
 (require 'dired-collapse)
 
 (add-hook 'dired-mode-hook #'dired-collapse-mode)
+
+(with-eval-after-load 'dired
+  (define-key dired-mode-map ")" 'dired-git-info-mode))
