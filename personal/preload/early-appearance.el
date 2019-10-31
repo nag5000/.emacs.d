723058e7437c3989a48f4054d50c8fa026a6d47e
nag5000 <al3x.nag@gmail.com>
2019-10-31 14:59:16 +0500

    Update

--- personal/preload/early-appearance.el
+++ personal/preload/early-appearance.el
@@ -1,10 +1,20 @@
 (setq prelude-theme 'spacemacs-light)
 
 ;; Set default font
+
+;; (set-face-attribute 'default nil
+;;                     :foundry "xos4"
+;;                     :family "xos4 Terminus"
+;;                     :height 120
+;;                     :weight 'normal
+;;                     :width 'normal
+;;                     :slant 'normal)
+
+;; https://input.fontbureau.com/preview/?size=10&language=javascript&theme=default&family=InputMono&width=200&weight=200&line-height=1&a=0&g=ss&i=topserif&l=serifs&zero=slash&asterisk=height&braces=0&preset=default&customize=please
 (set-face-attribute 'default nil
-                    :foundry "xos4"
-                    :family "xos4 Terminus"
-                    :height 120
+                    :foundry "FBI"
+                    :family "Input"
+                    :height 105 ;; 100, 110, 120
                     :weight 'normal
                     :width 'normal
                     :slant 'normal)
